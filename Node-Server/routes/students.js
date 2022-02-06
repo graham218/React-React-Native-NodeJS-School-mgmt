9776324c979ab9269793c2256db93f5ba63aca09
BILL GRAHAM <grahambill011@gmail.com>
2022-02-06 23:16:15 +0300

    updates

--- Node-Server/routes/students.js
+++ Node-Server/routes/students.js
@@ -39,46 +39,31 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
 //DELETE
 router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
-    await Product.findByIdAndDelete(req.params.id);
-    res.status(200).json("Product has been deleted...");
+    await Students.findByIdAndDelete(req.params.id);
+    res.status(200).json("This Student has been deleted from the database...");
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//GET PRODUCT
+//GET STUDENT
 router.get("/find/:id", async (req, res) => {
   try {
-    const product = await Product.findById(req.params.id);
-    res.status(200).json(product);
+    const student = await Students.findById(req.params.id);
+    res.status(200).json(student);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//GET ALL PRODUCTS
-router.get("/", async (req, res) => {
-  const qNew = req.query.new;
-  const qCategory = req.query.category;
-  try {
-    let products;
-
-    if (qNew) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(1);
-    } else if (qCategory) {
-      products = await Product.find({
-        categories: {
-          $in: [qCategory],
-        },
-      });
-    } else {
-      products = await Product.find();
+//GET ALL STUDENTS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+      const students = await Students.find();
+      res.status(200).json(students);
+    } catch (err) {
+      res.status(500).json(err);
     }
-
-    res.status(200).json(products);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+  });
 
 module.exports = router;
