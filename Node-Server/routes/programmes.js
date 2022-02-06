693cac783fe9b3e61576f94d5d2e67bd7422ad62
BILL GRAHAM <grahambill011@gmail.com>
2022-02-07 00:24:20 +0300

    updates

--- Node-Server/routes/programmes.js
+++ Node-Server/routes/programmes.js
@@ -1,4 +1,4 @@
-const NoticeBoard = require("../models/NoticeBoard");
+const Programmes = require("../models/Programmes");
 const {
   verifyToken,
   verifyTokenAndAuthorization,
@@ -10,11 +10,11 @@ const router = require("express").Router();
 //CREATE
 
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
-  const newNoticeBoard = new NoticeBoard(req.body);
+  const newProgrammes = new Programmes(req.body);
 
   try {
-    const noticeBoard = await newNoticeBoard.save();
-    res.status(200).json(noticeBoard);
+    const programmes = await newProgrammes.save();
+    res.status(200).json(programmes);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -23,14 +23,14 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
 //UPDATE
 router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
-    const updatedNoticeBoard = await NoticeBoard.findByIdAndUpdate(
+    const updatedProgrammes = await Programmes.findByIdAndUpdate(
       req.params.id,
       {
         $set: req.body,
       },
       { new: true }
     );
-    res.status(200).json(updatedNoticeBoard);
+    res.status(200).json(updatedProgrammes);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -39,28 +39,28 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
 //DELETE
 router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
-    await NoticeBoard.findByIdAndDelete(req.params.id);
-    res.status(200).json("This Notice has been deleted from the database...");
+    await Programmes.findByIdAndDelete(req.params.id);
+    res.status(200).json("This Programme has been deleted from the database...");
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//GET notice
+//GET programme
 router.get("/find/:id", async (req, res) => {
   try {
-    const notice = await NoticeBoard.findById(req.params.id);
-    res.status(200).json(notice);
+    const programme = await Programmes.findById(req.params.id);
+    res.status(200).json(programme);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//GET ALL notices
+//GET ALL programmes
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
     try {
-      const notice = await NoticeBoard.find();
-      res.status(200).json(notice);
+      const programmes = await Programmes.find();
+      res.status(200).json(programmes);
     } catch (err) {
       res.status(500).json(err);
     }
