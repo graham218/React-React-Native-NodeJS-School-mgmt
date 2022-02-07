1b640269d12ecc90a0fbc36340ea49ba53dbfcf5
BILL GRAHAM <grahambill011@gmail.com>
2022-02-07 13:51:27 +0300

    updates

--- Node-Server/index.js
+++ Node-Server/index.js
@@ -6,13 +6,23 @@ app.use(cors());
 const db = require("./models");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const adminRoute = require("./routes/admin");
+const facultyRoute = require("./routes/faculty");
+const feePaymentRoute = require("./routes/feePayment");
+const feeReceiptRoute = require("./routes/feeReceipt");
+const genderRoute = require("./routes/gender");
+const lecRoute = require("./routes/lecturers");
+const lecUnitsRoute = require("./routes/lecturerUnits");
+const marksyr1Route = require("./routes/marksyr1");
+const noticeRoute = require("./routes/noticeBoard");
+const programmesRoute = require("./routes/programmes");
 
 
 
 
 
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ /*force: true*/ }).then(() => {
     app.listen(3001, () => {
       console.log("Server running on port 3001");
     });
