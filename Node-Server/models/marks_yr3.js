324a62693db356c1a5b909aeb66e4826e1d7cf5b
BILL GRAHAM <grahambill011@gmail.com>
2022-02-07 14:16:22 +0300

    updates

---
+++ Node-Server/models/marks_yr3.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+    const marks_yr3 = sequelize.define("marks_yr3", {
+        user: {
+        type: DataTypes.STRING,
+      },
+      full_name: {
+        type: DataTypes.STRING,
+      },
+      stage: {
+        type: DataTypes.STRING,
+      },
+      unit_or_subject_name: {
+        type: DataTypes.STRING,
+      },
+      marks: {
+        type: DataTypes.STRING,
+      },
+      grade: {
+        type: DataTypes.STRING,
+      }
+    });
+    return marks_yr3;
+  };
+  
