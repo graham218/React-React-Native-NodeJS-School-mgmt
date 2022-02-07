d79d4df91b982586613308a773b1b799dc49b7dc
BILL GRAHAM <grahambill011@gmail.com>
2022-02-07 14:27:19 +0300

    updates

---
+++ Node-Server/routes/marksyr7.js
@@ -0,0 +1,69 @@
+const marks_yr6 = require("../models/marks_yr6");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+const router = require("express").Router();
+
+//CREATE
+
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newmarks_yr6 = new marks_yr6(req.body);
+
+  try {
+    const savedmarks_yr6 = await newmarks_yr6.save();
+    res.status(600).json(savedmarks_yr6);
+  } catch (err) {
+    res.status(600).json(err);
+  }
+});
+
+//UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedmarks_yr6 = await marks_yr6.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(600).json(updatedmarks_yr6);
+  } catch (err) {
+    res.status(600).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await marks_yr6.findByIdAndDelete(req.params.id);
+    res.status(600).json("This Marks have been deleted from the database...");
+  } catch (err) {
+    res.status(600).json(err);
+  }
+});
+
+//GET units
+router.get("/find/:id", async (req, res) => {
+  try {
+    const marks = await marks_yr6.findById(req.params.id);
+    res.status(600).json(marks);
+  } catch (err) {
+    res.status(600).json(err);
+  }
+});
+
+//GET ALL units
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+      const marks = await marks_yr7.find();
+      res.status(600).json(marks);
+    } catch (err) {
+      res.status(600).json(err);
+    }
+  });
+
+module.exports = router;
