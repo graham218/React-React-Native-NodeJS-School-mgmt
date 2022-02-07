d50f718c8f9f326674a622c404ea3699e64e2f2f
BILL GRAHAM <grahambill011@gmail.com>
2022-02-07 14:08:06 +0300

    updates

--- Node-Server/index.js
+++ Node-Server/index.js
@@ -16,9 +16,32 @@ const lecUnitsRoute = require("./routes/lecturerUnits");
 const marksyr1Route = require("./routes/marksyr1");
 const noticeRoute = require("./routes/noticeBoard");
 const programmesRoute = require("./routes/programmes");
+const specialExamRoute = require("./routes/specialExam");
+const stagesRoute = require("./routes/stages");
+const studentsRoute = require("./routes/students");
+const suggestionBoxRoute = require("./routes/suggestionBox");
+const unitsRoute = require("./routes/units");
+const votingRoute = require("./routes/voting");
 
 
-
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/admin", adminRoute);
+app.use("/api/v1/faculty", facultyRoute);
+app.use("/api/v1/feePayment", feePaymentRoute);
+app.use("/api/v1/feeReceipt", feeReceiptRoute);
+app.use("/api/v1/gender", genderRoute);
+app.use("/api/v1/lecturer", lecRoute);
+app.use("/api/v1/lecUnits", lecUnitsRoute);
+app.use("/api/v1/marksyr1", marksyr1Route);
+app.use("/api/v1/notice", noticeRoute);
+app.use("/api/v1/programmes", programmesRoute);
+app.use("/api/v1/specialExam", specialExamRoute);
+app.use("/api/v1/stages", stagesRoute);
+app.use("/api/v1/students", studentsRoute);
+app.use("/api/v1/suggestionBox", suggestionBoxRoute);
+app.use("/api/v1/units", unitsRoute);
+app.use("/api/v1/voting", votingRoute);
 
 
 
