4d8b8a3ede82bdd7fdefc3292a77ef7d3a11aa64
BILL GRAHAM <grahambill011@gmail.com>
2022-02-07 13:42:34 +0300

    u

--- Node-Server/index.js
+++ Node-Server/index.js
@@ -3,11 +3,12 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
+const db = require("./models");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 
 
-const db = require("./models");
+
 
 
 
