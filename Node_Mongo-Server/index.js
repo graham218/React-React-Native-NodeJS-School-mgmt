51d6a625085ead65b15b3972dd430bb189ab5817
graham218 <grahambill011@gmail.com>
2022-03-04 05:14:13 +0300

    updates

--- Node_Mongo-Server/index.js
+++ Node_Mongo-Server/index.js
@@ -28,11 +28,11 @@ ExpressPeerServer(http, { path: '/' })
 
 // Routes
 app.use('/api', require('./routes/authRouter'))
-app.use('/api', require('./routes/userRouter'))
-app.use('/api', require('./routes/postRouter'))
-app.use('/api', require('./routes/commentRouter'))
-app.use('/api', require('./routes/notifyRouter'))
-app.use('/api', require('./routes/messageRouter'))
+// app.use('/api', require('./routes/userRouter'))
+// app.use('/api', require('./routes/postRouter'))
+// app.use('/api', require('./routes/commentRouter'))
+// app.use('/api', require('./routes/notifyRouter'))
+// app.use('/api', require('./routes/messageRouter'))
 
 
 const URI = process.env.MONGODB_URL
